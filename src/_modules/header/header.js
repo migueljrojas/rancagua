5d03894b1424f556e98eeaacd244d0453fa1a3d4
Mike <migueljrojas@gmail.com>
2019-02-25 15:48:30 -0300

    header script

--- src/_modules/header/header.js
+++ src/_modules/header/header.js
@@ -5,10 +5,53 @@ var Header = function() {
     var header = $('.header');
     var body = $('body');
     var menuOpen = $('.header__hamburguer');
+    var url = window.location.pathname;
+    var urlRegExp = new RegExp(url == '/' ? window.location.origin + '/?$' : url.replace(/\/$/,'') + '$');
 
     menuOpen.on('click', function(){
         header.toggleClass('-open');
         body.toggleClass('-hideOverflow');
+    });
+
+    $('.header__menu li a').each(function(){
+        // and test its normalized href against the url pathname regexp
+
+        if(urlRegExp.test(this.href.replace(/\/$/,''))){
+            console.log(urlRegExp);
+
+            $(this).parents('.header__menu li').addClass('-active');
+        }
+    });
+
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+        // On-page links
+        if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+        &&
+        location.hostname == this.hostname
+        ) {
+        // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                scrollTop: target.offset().top + -110
+                }, 1000, function() {
+                // Callback after animation
+                // Must change focus!
+                var $target = $(target);
+                $target.focus();
+                console.log("shola");
+                });
+            }
+        }
     });
 };
 
